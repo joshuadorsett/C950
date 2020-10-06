dc077ac45e6f8447ce6346332eb96e4ab52b031b
joshuadorsett <joshdorsett@gmail.com>
2020-10-06 19:33:11 -0400

    second commit.

--- Main.py
+++ Main.py
@@ -1,9 +1,12 @@
-from Package import Package
-from HashTable import HashTable
+from Package import *
+from HashTable import *
 from reader import *
-Inventory = HashTable()
 
-p1 = Package(1, "195 W Oakland Ave", "Salt Lake City", 84115, "12/31/1899 10:30:00 AM", 21, "not delivered")
-Inventory.insert(p1.packageId, p1)
-print(Inventory.search(1).deliveryStatus)
-importAllData()
+if __name__ == "__main__":
+
+    Inventory = HashTable()
+
+    p1 = Package(1, "195 W Oakland Ave", "Salt Lake City", 84115, "12/31/1899 10:30:00 AM", 21, "not delivered")
+    Inventory.insert(p1.packageId, p1)
+    print(Inventory.search(1).deliveryStatus)
+    importAllData()
