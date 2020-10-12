3be9058ee46578479cb674f3bfc60063b0427178
joshuadorsett <joshdorsett@gmail.com>
2020-10-12 00:04:18 -0400

    fixed getSize methid in hashtable class to O(1)

--- DataStructures/HashTable.py
+++ DataStructures/HashTable.py
@@ -1,11 +1,14 @@
 
 
-# this class has a table of Key, Value buckets
+# this class has a table of bucket lists
+# each bucket list has a chain of buckets with key, value pairs
 # using a hash function it can search for a value with a key provided
 class HashTable:
     def __init__(self, startingCapacity=40):
         # initializes a list
         self._table = []
+        # initializes an integer to track size of occupied buckets
+        self._size = 0
         # sets each element of the list to an empty list or "bucket"
         for i in range(startingCapacity):
             self._table.append([])
@@ -18,17 +21,21 @@ class HashTable:
 
     # receives a key and a value and inserts into table
     def insert(self, key, value):
-        # creates a hashed key using this hash function
+        # creates a hashed key
         bucket = self.hashFunction(key)
+        # sets bucket list
         bucketList = self._table[bucket]
-        # the bucket is appended with new item
+        # the bucket list is appended with new item
         bucketList.append([key, value])
+        # increment size
+        self._size += 1
 
     # returns a value for the given key
     # time - O(1) - O(N)
     def getValue(self, key):
-        # creates a hashed key using this hash function
+        # creates a hashed key
         bucket = self.hashFunction(key)
+        # sets bucket list
         bucketList = self._table[bucket]
         # loop through bucket list
         for i in range(len(bucketList)):
@@ -38,16 +45,8 @@ class HashTable:
                 return bucketList[i][1]
 
     # returns the size of the table that is not set to None
-    # time - O(N)
     def getSize(self):
-        size = 0
-        # loops through table
-        for i in range(len(self._table)):
-            # if the tables is not set to None
-            if self._table[i] is not None:
-                # increments size
-                size += 1
-        return size
+        return self._size
 
     # deletes a bucket by setting it to None
     def delete(self, item):
@@ -57,3 +56,5 @@ class HashTable:
         # removes item from bucket list
         if item in bucketList:
             bucketList.remove(item)
+            # decrement size
+            self._size -= 1
